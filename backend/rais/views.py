c073c760f18d80f96b5f1edeec8ca6cf0628c872
Vladimir Morozov <vvovv266@gmail.com>
2020-11-11 11:39:40 +0700

    Registration and login added

--- backend/rais/views.py
+++ backend/rais/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+from rais.models import User
+import datetime
 
 
 # Create your views here.
@@ -28,9 +30,37 @@ def profile(request):
 
 @csrf_exempt
 def login(request):
-    print(request.body)
-    return render(request=request, template_name='rais/login.html')
+    # Если регистрируется новый пользователь
+    if len(request.POST) == 10:
+        user = User(name=request.POST['name'],
+                    surname=request.POST['surname'],
+                    username=request.POST['username'],
+                    country=request.POST['country'],
+                    town=request.POST['town'],
+                    birthdate=datetime.datetime.strptime(request.POST['birthdate'], '%Y-%m-%d').date(),
+                    phone=request.POST['phone'],
+                    email=request.POST['email'],
+                    password=request.POST['password']
+                    )
+        user.save()
+        return render(request=request, template_name='rais/login.html')
 
+    # Если зарегистрированный пользователь осуществляет вход
+    elif len(request.POST) == 2:
+        user = None
+        try:
+            user = User.objects.get(email=request.POST['email'])
+        # Если пользователя с такой почтой не существует
+        except User.DoesNotExist:
+            return render(request=request, template_name='rais/home.html')
+        else:
+            # Если пароль введён верно
+            if user.password == request.POST['password']:
+                return render(request=request, template_name='rais/login.html')
+            else:
+                return render(request=request, template_name='rais/home.html')
+    else:
+        return render(request=request, template_name='rais/home.html')
 
 def whatsinyourmind(request):
     return render(request=request, template_name='rais/whats-in-your-mind.html')
